9f630615201844d5ca6ba588a052993877c75a27
Siddhesh Nandoskar <sidnandoskar5@gmail.com>
2022-01-27 14:10:27 +0530

    Update Article.js
    fetching query param

--- react-router-multipage-site/src/pages/Article.js
+++ react-router-multipage-site/src/pages/Article.js
@@ -1,30 +1,39 @@
 import { useEffect } from "react"
-import { useParams, useHistory } from "react-router-dom"
+import { useHistory, useLocation, useParams } from "react-router-dom"
 import { useFetch } from "../hooks/useFetch"
 
 export default function Article() {
+
   const { id } = useParams()
   const url = `http://localhost:3000/articles/${id}`
-  const { data:article, isPending, error } = useFetch(url)
+  const {data:article, isPending, error} = useFetch(url)
   const history = useHistory()
+  
+  const queryString = useLocation().search
+  const queryParam = new URLSearchParams(queryString)
+  const name = queryParam.get('name')
+  const age = queryParam.get('age')
 
   useEffect(() => {
-    setTimeout(() => {
-      if(error){
+    if(error){
+      setTimeout(() => {
+        // history.goBack()
         history.push('/')
-      }
-    }, 2000)
+      }, 2000)
+    }
+
   },[error, history])
   
   return (
     <div>
-      {isPending && <p>Loading....</p>}
-      {error && <p>Unable to fetch data</p>}
-      {article && <div>
+      {name && name} {age && age}
+      {isPending && <p>Loading article...</p>}
+      {error && <p>{error}</p>}
+      {article && <article>
         <h2>{article.title}</h2>
         <p>{article.author}</p>
         <p>{article.body}</p>
-      </div>}
+      </article>}
     </div>
   )
 }
